53a58b44aee3b86a7e5d075dd2a4a88a6d3363ce
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2021-08-31 22:04:07 -0600

    Up and running, babyyyy

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof( Rigidbody2D))]
+public class Player: MonoBehaviour
+{   
+    public float speed = 6.0f; 
+    Rigidbody2D player;
+    
+    void Start()
+    {
+        player = gameObject.GetComponent<Rigidbody2D>();
+    } // Start is called before the first frame update
+
+    void Update()
+    {
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        player.MovePosition(player.position += (direction * speed * Time.deltaTime));
+        
+    } // Update is called once per frame
+} // End of class
