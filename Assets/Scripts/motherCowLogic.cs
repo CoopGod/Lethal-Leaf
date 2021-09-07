4da1ed5c4aa805bf0375dbef30a3a7c71a386fbe
Matt4970 <caron.matthew@yahoo.ca>
2021-09-07 17:31:27 -0600

    Edited Cow Script
    Added a circle collider for the player to interact with instead and the cow will immediately turn red when the player is in this zone. It looks stupid but I intend to add some coroutines that make the cow lerp in between the normal color and red so it slowly gets angry when the player is nearby, he will turn red at the same speed as the cows attack time.

--- Assets/Scripts/motherCowLogic.cs
+++ Assets/Scripts/motherCowLogic.cs
@@ -6,15 +6,23 @@ public class MotherCowLogic : LivingEntity
 {
     public float reactionTime = 0.75f;
     public float damage = 1;
-    float attackTime;
-    bool playerInRange = false;
+    public float attackRange = 10;
+    public float attackTime = 1;
+    public float collisionRadius;
+
+    bool playerInRange = false; 
 
     IDamageable player;
+    Transform target;
+    Collider2D circleCollider;
+    SpriteRenderer cowSprite;
 
     protected override void Start()
     {
         base.Start();
         player = FindObjectOfType<Player>().gameObject.GetComponent<IDamageable>();
+        target = FindObjectOfType<Player>().transform;
+        cowSprite = GetComponent<SpriteRenderer>();
     } // Start
 
     void Update()
@@ -33,14 +41,25 @@ public class MotherCowLogic : LivingEntity
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        float currentTime = Time.time;
         attackTime = Time.time + reactionTime;
         playerInRange = true;
-
+        cowSprite.color = Color.red;
     } // set attack time if player is in range
 
     void OnTriggerExit2D(Collider2D col)
     {
         playerInRange = false;
+        cowSprite.color = Color.white;
     } // let update know if the player has escaped the cow
-} 
+
+    // IEnumerator Attack()
+    // {
+
+    // } // Attack the player after changing color to show anger.
+
+    // IEnumerator CalmDown()
+    // {
+
+    // } // Revert the color back down so he calms down.
+
+} // End of class MotherCowLogic
