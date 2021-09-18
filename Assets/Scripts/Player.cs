2c9e2e19ae1fdc248b8b68409f32e39304623965
Matt4970 <caron.matthew@yahoo.ca>
2021-09-17 22:48:49 -0600

    Wall what the fuck

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,9 @@ public class Player: LivingEntity
         player = gameObject.GetComponent<Rigidbody2D>();
     } // Start is called before the first frame update
 
-    void Update()
+    void FixedUpdate()
     {
         Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.MovePosition(player.position += (direction * speed * Time.deltaTime)); 
-    } // Update is called once per frame
+    } // End of Fixed Update
 } // End of class 
