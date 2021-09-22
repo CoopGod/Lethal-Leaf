b55a7b79cf30532f71d8f2403cbb3e8afe961638
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2021-09-21 18:55:07 -0600

    Added cow radius thingy

--- Assets/Scripts/motherCowLogic.cs
+++ Assets/Scripts/motherCowLogic.cs
@@ -19,6 +19,7 @@ public class MotherCowLogic : MonoBehaviour
     CircleCollider2D circleCollider;
     SpriteRenderer cowSprite;
     public GameObject circleSprite;
+    public SpriteRenderer circleSpriteRenderer;
     Color originalColor;
     Color angryColor;
 
@@ -62,6 +63,7 @@ public class MotherCowLogic : MonoBehaviour
         while(angryPercent <= 1 && playerInRange)
         {
             cowSprite.color = Color.Lerp(originalColor, angryColor, angryPercent);
+            circleSpriteRenderer.color = new Color(0.2196079f, 0.2196079f, 0.2196079f, (angryPercent * 0.1f));
             angryPercent += Time.deltaTime * reactionTime;
             yield return null;
         } // Get angry when da human is intruding on his private time
@@ -78,6 +80,7 @@ public class MotherCowLogic : MonoBehaviour
         while(angryPercent >= 0 && !playerInRange)
         {
             cowSprite.color = Color.Lerp(originalColor, angryColor, angryPercent);
+            circleSpriteRenderer.color = new Color(0.2196079f, 0.2196079f, 0.2196079f, (angryPercent * 0.1f));
             angryPercent -= Time.deltaTime * reactionTime;
             yield return null;
         } // Calm down while the player is not near
